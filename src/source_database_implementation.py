e0864a57d6ce4a0a538c2b31ad002b16e32122ab
lknegendorf <Leonard.Knegendorf@outlook.com>
2021-02-06 19:33:35 +0100

    Update source_database_implementation.py
    Inserted def for connection to local database.

--- src/source_database_implementation.py
+++ src/source_database_implementation.py
@@ -10,7 +10,7 @@ import requests
 from google.colab import drive 
 drive.mount("/content/gdrive", force_remount=True)
 
-# SQLite connection
+# SQLite connection memory
 def create_connection_memory():
   conn = None;
   try:
@@ -20,6 +20,16 @@ def create_connection_memory():
     print(sqlite3.version)
   except Error as e:
     print("Error while connecting to sqlite", e)
+# SQLite connection definition local (needed later for public database)
+def create_connection_local(local_path):
+  conn = None;
+  try:
+    #Establishing the connection
+    conn = sq.connect(local_path+'/imecos_public.db')
+    return conn
+    print(sqlite3.version)
+  except Error as e:
+    print("Error while connecting to sqlite", e) 
 conn = create_connection_memory()
 # Creating a cursor object using the cursor() method
 cur = conn.cursor()
