af2de5b143ca4590ca31821553e5bbb9015601c6
lknegendorf <Leonard.Knegendorf@outlook.com>
2021-02-08 10:50:21 +0100

    Included hash sum calculation
    Hash sums will be displayed when loading Data Warehouse via ublic_datawarehouse_connection.py
    Hash sums can be verified by comparison with table in [Wiki/SYSTEM-UMGEBUNG](https://github.com/Fuenfgeld/ATeamDatenmanagementUndArchivierung/wiki/System-Umgebung) #44

--- src/public_datawarehouse_connection.py
+++ src/public_datawarehouse_connection.py
@@ -34,6 +34,29 @@ conn_new = create_connection_local('/content/gdrive/MyDrive')
 # Creating a cursor object using the cursor() method
 cur_new = conn_new.cursor()
 print("Successfully Connected to SQLite Public Data Warehouse")
+
+# calculate and display hashsum for each table
+from pandas.util import hash_pandas_object
+df_encounters = pd.read_sql_query("SELECT * FROM encounters", conn_new)
+encounterDFhashes = hash_pandas_object(df_encounters)
+del(df_encounters)
+print('encounters : ', encounterDFhashes.sum())
+del(encounterDFhashes)
+df_patients = pd.read_sql_query("SELECT * FROM patients", conn_new)
+patientsDFhashes = hash_pandas_object(df_patients)
+del(df_patients)
+print('patients : ', patientsDFhashes.sum())
+del(patientsDFhashes)
+df_payers = pd.read_sql_query("SELECT * FROM payers", conn_new)
+payersDFhashes = hash_pandas_object(df_payers)
+del(df_payers)
+print('payers : ', payersDFhashes.sum())
+del(payersDFhashes)
+df_code_master = pd.read_sql_query("SELECT * FROM code_master", conn_new)
+code_masterDFhashes = hash_pandas_object(df_code_master)
+del(df_code_master)
+print('code_master : ', code_masterDFhashes.sum())
+del(code_masterDFhashes)
 print("""Research database ready for work
   
       conn_new    -    is the name of the connection object
